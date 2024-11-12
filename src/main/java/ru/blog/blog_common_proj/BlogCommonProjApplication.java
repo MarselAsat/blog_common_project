72aa71b443324531d44d380e1f2fa9e85f33f5a6
Marsel.Asatov <asamarsel@mail.ru>
2024-11-12 13:30:12 +0300

    first

---
+++ src/main/java/ru/blog/blog_common_proj/BlogCommonProjApplication.java
@@ -0,0 +1,13 @@
+package ru.blog.blog_common_proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCommonProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogCommonProjApplication.class, args);
+    }
+
+}
